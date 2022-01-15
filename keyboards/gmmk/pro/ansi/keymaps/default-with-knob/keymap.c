fada7ca8f4e9401a9c7c7d71f5baa694029d22b5
ramonvg <ramonvillalongaa@gmail.com>
2022-01-15 23:55:45 +0100

    wip

--- keyboards/gmmk/pro/ansi/keymaps/default-with-knob/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/default-with-knob/keymap.c
@@ -18,18 +18,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum custom_keycodes {
     GIT_COMMIT = SAFE_RANGE,
+    JUMP_AUDUR,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case GIT_COMMIT:
-        if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
-            SEND_STRING("git add . && git commit -m wip\n");
-        } else {
-            // when keycode QMKBEST is released
-        }
-        break;
+        case GIT_COMMIT:
+            if (record->event.pressed) {
+                // when keycode QMKBEST is pressed
+                SEND_STRING("git add . && git commit -m wip\n");
+            }
+            break;
+        
+        case JUMP_AUDUR:
+            if (record->event.pressed) {
+                SEND_STRING("jump audur-api");
+            }
+            break;
     }
     return true;
 };
@@ -60,16 +65,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_MPLY,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_END,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
+        LT(2, KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGUP,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT(
-        GIT_COMMIT, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV,  KC_MNXT,  KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_INS,           KC_MUTE,
+        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV,  KC_MNXT,  KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_INS,           KC_MUTE,
         _______, RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______,          _______,
         _______, _______, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI,  BL_TOGG,  BL_BRTG, BL_ON,   BL_INC,  _______, _______, _______, RESET,            _______,
         _______, _______, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  BL_STEP,  _______, BL_OFF,  BL_DEC,  _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______,  _______,  NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
+        _______, _______, _______,                             _______,                             _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+    ),
+
+
+    [2] = LAYOUT(
+        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV,  KC_MNXT,  KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_INS,           KC_MUTE,
+        _______, GIT_COMMIT, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______,          _______,
+        _______, _______, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI,  BL_TOGG,  BL_BRTG, BL_ON,   BL_INC,  _______, _______, _______, RESET,            _______,
+        _______, JUMP_AUDUR, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  BL_STEP,  _______, BL_OFF,  BL_DEC,  _______, _______,          _______,          _______,
         _______,          _______, _______, _______, _______,  _______,  NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
         _______, _______, _______,                             _______,                             _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
